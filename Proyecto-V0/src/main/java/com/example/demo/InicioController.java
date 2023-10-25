6d64cbfccc77bcdec0a1f6df1783b2c87599b982
Oscar <oscargimenosa@gmail.com>
2023-10-25 16:33:42 +0200

    Ficheros iniciales

---
+++ Proyecto-V0/src/main/java/com/example/demo/InicioController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+
+public class InicioController {
+
+	@GetMapping(value = "inicio", produces = MediaType.TEXT_PLAIN_VALUE)
+
+	public String PaginaInicio() 
+
+	{
+
+		return "About us    Contacto    Iniciar sesion";
+
+	}
+
+}
---
+++ Proyecto-V0/src/main/java/com/example/demo/ProyectoV0Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoV0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoV0Application.class, args);
+	}
+
+}
